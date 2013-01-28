b20cc95e79ce9421409316d65f652da58062ce79
Joshua Montross <joshua.montross@gmail.com>
2013-01-28 10:43:48 -0800

    ads z.sh - z is the new j yo

--- attributes/rvm.rb
+++ attributes/rvm.rb
@@ -6,6 +6,7 @@ node.default["rvm"]["rubies"] = {
   #{}"ree-1.8.7-2012.02" => { :env => "CC=gcc-4.2" },
   #"ruby-1.8.7-p358" => { :env => "CC=gcc-4.2" },
   #{}"ruby-1.9.2-p290" => { :env => "CC=gcc-4.2" },
+  "ruby-1.8.7-p358" => { :env => "CC=/opt/local/bin/gcc-apple-4.2", :command_line_options => "--with-gcc=/opt/local/bin/gcc-apple-4.2 --with-openssl-dir=/opt/local/" },
   "jruby-1.7.0" => { :command_line_options => "--with-gcc=clang" },
   "ruby-1.9.3-p286" => { :command_line_options => "--with-gcc=clang" },
   "ruby-1.9.3-p194" => { :command_line_options => "--with-gcc=clang" }
